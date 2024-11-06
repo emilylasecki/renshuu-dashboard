132568a82fb831547f491048f6e8f0978151bab3
Emily Lasecki <emilylasecki@oakland.edu>
2024-11-06 17:43:42 -0500

    added ability to fetch profile and schedule json objects

--- main.py
+++ main.py
@@ -7,50 +7,51 @@ import pandas as pd
 from api_key import api_key
 
 
-#objective
+#connect to Resnhuu server and load in content as needed
 def main():
 
     headers = {
         'accept': 'application/json',
         'Authorization': 'Bearer ' + api_key
     }   
+    url = 'https://api.renshuu.org/v1/profile/'  # get the number of reviews due today
+    url2 = 'https://api.renshuu.org/v1/schedule/' # get the lists to extract how many of each type of review is due
 
-    #endpoint = '/v1/profile'
-    url = 'https://api.renshuu.org/v1/profile'
+    # part 1 - profile
 
     response = requests.get(url, headers=headers)
-    #print(response)
 
     print(response.status_code)
     x = response.json()
-    #y = json.loads(x)
     print(json.dumps(x, indent=4, sort_keys=True))
 
-    filter_fields=['adventure_level', 'real_name', 'studied', 'api_usage']
+    filter_fields=['adventure_level', 'real_name', 'studied', 'api_usage', 'studied']
 
     dict_result = { key: x[key]  for key in x if key in filter_fields}
 
-    toExcel = { key: x[key]  for key in x }
-
-    with open("API_stuff.json", "w") as outfile:  # extract desired fields into json file
+    with open("profile.json", "w") as outfile:  # extract desired fields into json file
         json.dump(dict_result, outfile)
 
     
-    df = pd.read_json("API_stuff.json", lines=True)
+    df = pd.read_json("profile.json", lines=True)
     df.to_excel("API_Stuff.xlsx")
 
     print(dict_result)
 
-   # if key in x if in key in filter_fields:
-    #    print(key, x)
+    # part 2 - schedules
 
-    #print(y["adventure_level"])
-    #print(response.json())
+    response = requests.get(url2, headers=headers)
+    x = response.json()
+    print(json.dumps(x, indent=4, sort_keys=True))
+    filter_fields=['schedules', 'today']
+    dict_result = { key: x[key]  for key in x if key in filter_fields}
 
+    with open("schedules.json", "w") as outfile:  # extract desired fields into another json file
+        json.dump(dict_result, outfile)
 
 # reload stats upon click
+# placeholder method
 def reload():
-    print("place holder")
 
     headers = {
         'accept': 'application/json',
