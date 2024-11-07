ce197fc65e3ed7d0b2d20040b6fbcb5e6531b5ca
Emily Lasecki <emilylasecki@oakland.edu>
2024-11-07 16:55:58 -0500

    configured renshuu font

--- frame.py
+++ frame.py
@@ -16,6 +16,23 @@ img = ImageTk.PhotoImage(img)
 panel = tk.Label(window, image = img, bd=0)
 panel.image = img
 panel.grid(row =2)
+
+#label = tk.Label(window, text="renshuu")
+#label.grid()
+
+#text = tk.Text(window)
+#myFont = tk.Font(family="UD Digi Kyokasho", size=12)
+#text.configure(font=myFont)
+
+l = tk.Label(window, text = "renshuu", width=10, height=2, font="UD_Digi_Kyokasho")
+l.grid()
+#sample_text = tk.Text( window, height = 10) 
+#sample_text.pack() 
+#test = ("UD Digi Kyokasho", 20, "bold")
+#test.grid()
+#window.text = tk.text(window, text="test", fill="white", font='UD Digi Kyokasho')
 #button = tk.Label(window, image = img)
 #button.pack(pady=20)
+#sample_text.configure(font = Font_tuple) 
+#window.txt.insert(0, "test")
 window.mainloop()
