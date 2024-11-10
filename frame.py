c29c8d1ed4516f42be8bf458a5503d0c8a303e91
Emily Lasecki <emilylasecki@oakland.edu>
2024-11-09 19:43:50 -0500

    added temporary settings button

--- frame.py
+++ frame.py
@@ -21,27 +21,46 @@ character = tk.PhotoImage(file="myKao.png")
 imagetest = (Image.open("myKao.png"))
 img = imagetest.resize((100,100), Image.Resampling.LANCZOS)
 img = ImageTk.PhotoImage(img)
-canvas.create_image(50,50,image=img)  # need to either make all images square and a certain dimension or get this to scale based on the image
-
-
-
-"""
-#insert image
-path = "myKao.png"
-#path ="https://www.renshuu.org/img/adventure/f/nryjgbz_100_20240408.png" # use path of user's kao-chan on GUI?
-imagetest = (Image.open(path))
-img = imagetest.resize((70,70), Image.Resampling.LANCZOS)
-img = ImageTk.PhotoImage(img)
-panel = tk.Label(window, image = img, bd=0)
-panel.image = img
-panel.grid(row=0, column=0) """
+canvas.create_image(50,50,image=img)  # potential change, image is squashed. Acceptable the way it is but could look better
 
 #title
 l = tk.Label(window, bg="#1c5669", text = "renshuu dashboard \n 毎日がんばってね!", width=15, height=0, font="UD_Digi_Kyokasho", fg="white", anchor="nw")
 l.config(font=("UD_Digi_Kyokasho", 20, "bold")) # to bold or not to bold?
 l.grid(row=0,column=1)
 
+#imagetest = (Image.open("kaochan.jpg"))
+#img = imagetest.resize((70,70), Image.Resampling.LANCZOS)
+#img = ImageTk.PhotoImage(imagetest)
+#panel = tk.Label(window, image = img, bd=0)
+#canvas.image = img
+#canvas.grid(row=0, column=3)
+
+frame =tk.Frame(window, bg="#1c5669", borderwidth=0, highlightthickness=0)
+frame.grid(row=0, column=3)
+
+canvas2 = tk.Canvas(frame, bg="orange", width=130, height=100, borderwidth=0, highlightthickness=0)
+#canvas2.grid()
+
+# settings button
+#FIXME left off here
+img2 = tk.PhotoImage(file="settings.png")
+imagetest2 = (Image.open("settings.png"))
+img2 = imagetest2.resize((30,30), Image.Resampling.LANCZOS)
+img2 = ImageTk.PhotoImage(img2)
+canvas2.create_image(30,30, image=img2)
+canvas2.grid()
+
+
+frame2 =tk.Frame(window, bg="#201c1c", borderwidth=5, highlightthickness=0, width=425, height=340)
+frame2.place(relx=0.5, rely=0.6, anchor=tk.CENTER) # kissing my dreams of rounded corners goodbye
+
+
+# now alter here depending on what view is selected
+
+#canvas = tk.Canvas(frame2, bg="#1c5669", width=100, height=100, borderwidth=0, highlightthickness=0)
+#canvas.grid()
 #headings
+"""
 l = tk.Label(window, text = "Today's goals:", width=15, height=2, font="UD_Digi_Kyokasho", bg="grey", fg="black", anchor="w")
 l.config(font=("UD_Digi_Kyokasho", 12, "bold")) 
 l.grid(row=1, column=1)
@@ -49,15 +68,11 @@ l = tk.Label(window, text = "Studied Today:", width=15, height=2, font="UD_Digi_
 l.config(font=("UD_Digi_Kyokasho", 12, "bold")) 
 l.grid(row=1, column=2)
 
-l = tk.Label(window, text = " ", width=6, height=2, font="UD_Digi_Kyokasho", bg="grey", fg="black", anchor="nw")
-l.config(font=("UD_Digi_Kyokasho", 12, "bold")) 
-l.grid(row=1, column=0)
-
 l = tk.Label(window, text = "Vocab:", width=15, height=2, font="UD_Digi_Kyokasho", bg="grey", fg="black", anchor="nw")
 l.config(font=("UD_Digi_Kyokasho", 12, "bold")) 
 l.grid(row=2, column=1)
 
 l = tk.Label(window, text = "Vocab:", width=15, height=2, font="UD_Digi_Kyokasho", bg="grey", fg="black", anchor="nw")
 l.config(font=("UD_Digi_Kyokasho", 12, "bold")) 
-l.grid(row=2, column=2) 
+l.grid(row=2, column=2) """
 window.mainloop()
